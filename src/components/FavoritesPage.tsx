b570b09775992f380b4562a8064f8cc8388e53fb
Jacob Coffey <Jacob23.coffey@gmail.com>
2022-11-01 22:04:05 -0400

    Push to update main before merge

--- src/components/FavoritesPage.tsx
+++ src/components/FavoritesPage.tsx
@@ -10,12 +10,13 @@ export const FavoritesPage = () => {
 
     return (
         <div className="favorite-container">
-            {favoriteMovies.length === 0 && <h2>You currently have no favorite movies!</h2>}
+            {favoriteMovies.length === 0 && <><br></br>
+            <h2>You currently have no favorite movies!</h2></>}
          {favoriteMovies.map((movie, i) => {
              const sucks = movie.vote_average <= 6;
              const average = 6 < movie.vote_average && movie.vote_average <= 7;
              const great = movie.vote_average > 7;
-            return <div className="favorite" key={i}>
+            return  <div className="favorite" key={i}>
                     <h3 key={i}>{movie.title}</h3>
                     <img src={imageSrc + movie.poster_path} key={i}></img><br></br>
                     {sucks && <span id="star">☆</span>}
@@ -23,7 +24,7 @@ export const FavoritesPage = () => {
                     {great && <><span id="star">☆</span><span id="star">☆</span><span id="star">☆</span></>}
                     <br></br>
                     <button onClick={() => removeMovie(movie.title)} key={i}>Remove from Favorites</button>
-                   </div>
+                    </div>
          })}
         </div>
     )
